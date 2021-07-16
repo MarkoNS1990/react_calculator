f32af79dd0623f673841ae23dbf998767b61bb44
Marko Zecevic <marczecevic@gmail.com>
2021-07-16 15:24:59 +0200

    Implement Logic in Display component

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+function Display({result}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Display.defaultProps = {
+    result: 0
+}
+
+Display.propTypes = {
+    result: PropTypes.string.isRequired
+}
+
+export default Display
