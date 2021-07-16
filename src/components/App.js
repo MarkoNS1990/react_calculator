ffbbd0d44e540408014ba334e8b50eb04b681ce0
Marko Zecevic <marczecevic@gmail.com>
2021-07-16 15:20:20 +0200

    Add Button,ButtonPanel and Display Components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import Display from './Display'
+import ButtonPanel from './ButtonPanel'
+function App() {
+  return (
+    <>
+      <h1>Calculator with React. </h1>
+      <Display />
+      <ButtonPanel />
+    </>
+  );
+}
+
+export default App;
